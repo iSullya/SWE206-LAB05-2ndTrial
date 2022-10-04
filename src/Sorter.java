1c5eb0d03582746227891b0ecbf6b85d4df9becc
iSullya <czxcz.11@gmail.com>
2022-10-04 18:47:55 +0300

    insertionSort added

--- src/Sorter.java
+++ src/Sorter.java
@@ -9,7 +9,7 @@ public class Sorter {
             int j = step - 1;
 
             // Compare key with each element on the left of it until an element smaller than
-            // it is found.
+
             // For descending order, change key<array[j] to key>array[j].
             while (j >= 0 && key < array[j]) {
                 array[j + 1] = array[j];

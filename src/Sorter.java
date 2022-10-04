bed36663167e3c3130eadb084e9c77f0aad145a0
iSullya <czxcz.11@gmail.com>
2022-10-04 19:39:57 +0300

    insertionSort added

--- src/Sorter.java
+++ src/Sorter.java
@@ -12,6 +12,26 @@ public class Sorter {
             array[i] = temp;
         }
     }
+
+    void insertionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 1; step < size; step++) {
+            int key = array[step];
+            int j = step - 1;
+
+            // Compare key with each element on the left of it until an element smaller than
+            // it is found.
+            // For descending order, change key<array[j] to key>array[j].
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+
+            // Place key at after the element just smaller than it.
+            array[j + 1] = key;
+        }
+    }
     // Method to print the elements of an array
     void printArray(int[] array)
     {
@@ -24,8 +44,12 @@ public class Sorter {
     {
         Sorter ob = new Sorter();
         int[] array = {15, 10, 99, 53, 36};
+        int[] array2 = {2,1,4,3};
+
+        ob.insertionSort(array2);
         ob.selectionSort(array);
         System.out.println("Sorted array");
         ob.printArray(array);
+        ob.printArray(array2);
     }
 }

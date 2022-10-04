cf56ca8b5ffd82eadf471833f6b25c2c27fefd97
iSullya <czxcz.11@gmail.com>
2022-10-04 18:39:28 +0300

    test2

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,7 +1,35 @@
+import java.util.Arrays;
+
 public class Sorter {
+    void insertionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 1; step < size; step++) {
+            int key = array[step];
+            int j = step - 1;
+
+            // Compare key with each element on the left of it until an element smaller than
+            // it is found.
+            // For descending order, change key<array[j] to key>array[j].
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+
+            // Place key at after the element just smaller than it.
+            array[j + 1] = key;
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("test");
-        //
+        int s[] = {2,1,4,3};
+        System.out.println(Arrays.toString(s));
+
+        Sorter is = new Sorter();
+        is.insertionSort(s);
+        System.out.println(Arrays.toString(s));
+
+
+
     }
 }
